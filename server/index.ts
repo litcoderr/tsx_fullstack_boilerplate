809722169448d5a58ce2956bc935ec719760ce5c
지영채 <2019017183@hanyang.ac.kr>
2021-01-11 05:36:31 +0900

    master: finished implementing express and react boilerplate

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+import express, {Application, Request, Response} from "express";
+
+const app: Application = express();
+
+app.use(express.static(__dirname + "/../client/dist"));
+
+app.get('/', (req: Request, res: Response) => {
+    res.render("index.html");
+});
+
+app.listen(5000, () => {
+    console.log("Server Running @ 5000");
+});
