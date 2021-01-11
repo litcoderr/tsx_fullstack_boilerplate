638475057f6d54285a8857be08bcb2f778334a3b
지영채 <2019017183@hanyang.ac.kr>
2021-01-12 01:45:01 +0900

    master: some-how not modified. modifying changes now

---
+++ client/index.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<>Hello World</>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'client') + '/index.ts',
+    entry: path.resolve(__dirname, 'client') + '/index.tsx',
     module: {
         rules: [{
             test: /\.tsx?$/,
