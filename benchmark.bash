76cb2bec5b061c356a894b241c52c32a3e6125df
Youngchae James Chee <litcoderr@gmail.com>
2021-07-13 22:24:03 +0900

    master: added benchmark copying script for easier transfer

---
+++ benchmark.bash
@@ -0,0 +1,6 @@
+
+if [ -z $1 ]; then
+	echo "No target directory. Pass in target directory as an argument"
+else
+	rsync -av --progress . $1 --exclude node_modules --exclude .git --exclude benchmark.bash
+fi
